4d05e36858291fc90cfdff04aace9d590595b50b
Artem Batura <artemir.q@gmail.com>
2020-12-25 23:27:00 +0300

    Update modify-source-webpack-plugin to latest version Bump version

--- src/HotAcceptPlugin.ts
+++ src/HotAcceptPlugin.ts
@@ -1,47 +1,55 @@
-import { ModifyModuleSourcePlugin, Option } from 'modify-source-webpack-plugin';
+import { ModifySourcePlugin, Rule } from 'modify-source-webpack-plugin';
 import type { compilation } from 'webpack';
 
 export type Options = {
   test: string | RegExp | (string | RegExp)[];
+  debug?: boolean;
 };
 
 interface NormalModule extends compilation.Module {
   request?: string;
+  userRequest?: string;
+  rawRequest?: string;
   loaders?: Array<{
     loader: string;
     options?: any;
   }>;
 }
 
-export class HotAcceptPlugin extends ModifyModuleSourcePlugin {
+function createRule(test: string | RegExp, modify: Rule['modify']): Rule {
+  if (typeof test === 'string') {
+    const testFn = (module: NormalModule) => {
+      if (module.userRequest) {
+        return module.userRequest.endsWith(test);
+      }
+
+      return false;
+    };
+
+    return {
+      test: testFn,
+      modify
+    };
+  }
+
+  return {
+    test,
+    modify
+  };
+}
+
+export class HotAcceptPlugin extends ModifySourcePlugin {
   constructor(options: Options) {
-    const modify: Option['modify'] = src =>
+    const modify = (src: string) =>
       src + 'if (module.hot) { module.hot.accept(); }';
 
-    const arg = ([] as Array<Options['test']>)
-      .concat(options.test)
-      .map(optionTest => {
-        if (typeof optionTest === 'string') {
-          const test = (module: NormalModule) => {
-            if (module.request) {
-              return module.request.endsWith(optionTest);
-            }
+    const parentOptions = {
+      rules: Array.isArray(options.test)
+        ? options.test.map(test => createRule(test, modify))
+        : [createRule(options.test, modify)],
+      debug: options.debug
+    };
 
-            return false;
-          };
-
-          return {
-            modify,
-            test
-          };
-        }
-
-        return {
-          modify,
-          test: optionTest as RegExp
-        };
-      });
-
-    super(arg);
+    super(parentOptions);
   }
 }
